de90c15d9b6304ccef97e5705c6f287fd5c52f48
opedrodev <pvasilva.dev@hotmail.com>
2022-12-05 12:07:26 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles/App.module.scss';
+
+const App = () => {
+  return (
+    <div className={ styles.app }>Todo List</div>
+  );
+};
+
+export default App;
